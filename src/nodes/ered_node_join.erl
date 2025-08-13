32c384e7b440ec171d8f0e3d7f83c4accdf411da
Gerrit Riessen <gerrit@flowhub.org>
2025-08-13 09:44:30 +0200

    join node: support the 'use existing parts' option of the manual mode

--- src/nodes/ered_node_join.erl
+++ src/nodes/ered_node_join.erl
@@ -49,22 +49,31 @@
 
 %%
 %%
+start(#{<<"timeout">> := Timeout} = NodeDef, WsName) when Timeout =/= <<>> ->
+    ErrMsg = jstr("Timeout ~p", [NodeDef]),
+    unsupported(NodeDef, {websocket, WsName}, ErrMsg),
+    ered_node:start(NodeDef, ered_node_ignore);
 start(
     #{
-        <<"mode">> := Mode,
-        <<"build">> := Build,
-        <<"count">> := Count,
-        <<"propertyType">> := PropType
+        <<"mode">> := <<"custom">>,
+        <<"build">> := <<"array">>,
+        <<"count">> := Count
     } = NodeDef,
     WsName
 ) ->
-    case use_manual(Mode, Build, PropType, Count, NodeDef) of
-        {ok, V} ->
-            ered_node:start(V, ?MODULE);
-        {false, V} ->
+    case setup_custom(NodeDef) of
+        {ok, NodeDef2} ->
+            ered_node:start(
+                NodeDef2#{
+                    '_store' => [],
+                    '_count' => convert_to_int(Count)
+                },
+                ?MODULE
+            );
+        {false, NodeDef2} ->
             ErrMsg = jstr("Node Config ~p", [NodeDef]),
             unsupported(NodeDef, {websocket, WsName}, ErrMsg),
-            ered_node:start(V, ered_node_ignore)
+            ered_node:start(NodeDef2, ered_node_ignore)
     end;
 start(NodeDef, WsName) ->
     ErrMsg = jstr("Node Config ~p", [NodeDef]),
@@ -78,91 +87,85 @@ handle_event(_, NodeDef) ->
 
 %%
 %%
-%% Handle manually collecting a specific property name.
-%% Just received the final message, ready to generated a message
 handle_msg(
-    {incoming, Msg},
+    {incoming, #{<<"complete">> := true} = Msg},
     #{
-        '_is_manually_collecting' := {1, {prop, PropName}, Lst},
-        <<"count">> := Count
+        '_store' := Store,
+        <<"useparts">> := false
     } = NodeDef
 ) ->
-    %% because these values are sent off to the complete node,
-    %% need to keep a copy of the original message.
-    Lst2 = [{retrieve_prop_value(PropName, Msg), Msg} | Lst],
-
-    %% now that we are ready to send out our message, we are completed
-    %% with the message that make up that message (!!) so those
-    %% messages should be sent to a complete node - if there is one
-    %% See this post for details:
-    %%   https://discourse.nodered.org/t/complete-node-msg-before-or-after-computation/96648/5
-    [post_completed(NodeDef, M) || {_, M} <- lists:reverse(Lst2)],
-
-    %% Need to reverse the order of the returned array - because
-    %% we've been pushing onto the head and not the tail.
-    Msg2 = Msg#{?AddPayload([V || {V, _} <- lists:reverse(Lst2)])},
-    send_msg_to_connected_nodes(NodeDef, Msg2),
-
-    {ok, Cnt} = convert_to_int(Count),
-    %% reset the counter, ready to receive more messages
-    NodeDef2 =
-        NodeDef#{'_is_manually_collecting' => {Cnt, {prop, PropName}, []}},
-
+    NodeDef2 = send_out_collected_messages(NodeDef, [Msg | Store]),
     {handled, NodeDef2, dont_send_complete_msg};
-%% Not yet collected all messages, add message to list
+handle_msg(
+    {incoming,
+        #{
+            <<"complete">> := true,
+            <<"parts">> := #{<<"id">> := IdStr}
+        } = Msg},
+    #{
+        '_store' := Store,
+        <<"useparts">> := true
+    } = NodeDef
+) ->
+    {WithId, WithoutId} = divide_and_conquer(IdStr, [Msg | Store]),
+
+    send_out_collected_messages(NodeDef, lists:reverse(WithId)),
+
+    {handled, NodeDef#{'_store' => WithoutId}, dont_send_complete_msg};
+handle_msg(
+    {incoming, #{<<"complete">> := true} = Msg},
+    #{
+        '_store' := Store,
+        '_count' := Count,
+        <<"useparts">> := true
+    } = NodeDef
+) when Count =< 0 ->
+    {WithId, WithoutId} = divide_and_conquer_no_idstr([Msg | Store]),
+
+    send_out_collected_messages(NodeDef, lists:reverse(WithoutId)),
+
+    {handled, NodeDef#{'_store' => WithId}, dont_send_complete_msg};
+handle_msg(
+    {incoming, #{<<"complete">> := true} = Msg},
+    #{
+        '_store' := Store,
+        '_count' := Count,
+        <<"useparts">> := true
+    } = NodeDef
+) when Count > 0 ->
+    NodeDef2 = send_out_collected_messages(NodeDef, [Msg | Store]),
+    {handled, NodeDef2, dont_send_complete_msg};
 handle_msg(
     {incoming, Msg},
     #{
-        '_is_manually_collecting' := {Cnt, {prop, PropName}, Lst}
+        '_store' := Store,
+        '_count' := Count,
+        <<"useparts">> := true
     } = NodeDef
-) ->
-    NodeDef2 = NodeDef#{
-        '_is_manually_collecting' =>
-            {Cnt - 1, {prop, PropName}, [
-                {retrieve_prop_value(PropName, Msg), Msg} | Lst
-            ]}
-    },
+) when Count =< 0 ->
+    %% No count set, but using parts - so we have to check whether all parts
+    %% have arrived, if so then send out the parts/messages. If not, it's
+    %% back to the daily grind.
+    NodeDef2 = have_all_parts_arrived(NodeDef, [Msg | Store]),
+    {handled, NodeDef2, dont_send_complete_msg};
+handle_msg(
+    {incoming, Msg},
+    #{
+        '_store' := Store,
+        '_count' := Count
+    } = NodeDef
+) when Count > 0, length(Store) =:= (Count - 1) ->
+    NodeDef2 = send_out_collected_messages(NodeDef, [Msg | Store]),
     {handled, NodeDef2, dont_send_complete_msg};
 %%
-%% Collecting the entire Message object
-%%
-%% Recieved final message
+%% If we make it here, then just store the message and move on.
 handle_msg(
     {incoming, Msg},
     #{
-        '_is_manually_collecting' := {1, {entire_msg}, Lst},
-        <<"count">> := Count
+        '_store' := Store
     } = NodeDef
 ) ->
-    Lst2 = [Msg | Lst],
-
-    %% now that we are ready to send out our message, we are completed
-    %% with the message that make up that message (!!) so those
-    %% messages should be sent to a complete node - if there is one
-    %% See this post for details:
-    %%   https://discourse.nodered.org/t/complete-node-msg-before-or-after-computation/96648/5
-    [post_completed(NodeDef, M) || M <- lists:reverse(Lst2)],
-
-    %% Need to reverse the order of the returned array - because
-    %% we've been pushing onto the head and not the tail.
-    Msg2 = Msg#{?AddPayload(lists:reverse(Lst2))},
-    send_msg_to_connected_nodes(NodeDef, Msg2),
-
-    {ok, Cnt} = convert_to_int(Count),
-
-    NodeDef2 = NodeDef#{'_is_manually_collecting' => {Cnt, {entire_msg}, []}},
-    {handled, NodeDef2, dont_send_complete_msg};
-handle_msg(
-    {incoming, Msg},
-    #{
-        '_is_manually_collecting' := {Cnt, {entire_msg}, Lst}
-    } = NodeDef
-) ->
-    NodeDef2 = NodeDef#{
-        '_is_manually_collecting' =>
-            {Cnt - 1, {entire_msg}, [Msg | Lst]}
-    },
-    {handled, NodeDef2, dont_send_complete_msg};
+    {handled, NodeDef#{'_store' => [Msg | Store]}, dont_send_complete_msg};
 %%
 %%
 handle_msg(_, NodeDef) ->
@@ -171,58 +174,139 @@ handle_msg(_, NodeDef) ->
 %%
 %%
 %% --------------- helpers
+
+%%
+%%
+have_all_parts_arrived(
+    NodeDef,
+    [#{<<"parts">> := #{<<"count">> := Count}} | _] = AllMsgs
+) ->
+    have_all_parts_arrived(
+        NodeDef,
+        AllMsgs,
+        length(AllMsgs) =:= convert_to_int(Count)
+    );
+have_all_parts_arrived(#{'_store' := Store} = NodeDef, [LastMsg | _]) ->
+    NodeDef#{'_store' => [LastMsg | Store]}.
+%%
+have_all_parts_arrived(#{'_store' := Store} = NodeDef, [LastMsg | _], false) ->
+    NodeDef#{'_store' => [LastMsg | Store]};
+have_all_parts_arrived(NodeDef, AllMsgs, true) ->
+    send_out_collected_messages(NodeDef, AllMsgs).
+
+%%
+%%
+send_out_collected_messages(
+    #{<<"propertyType">> := <<"full">>} = NodeDef,
+    Store
+) ->
+    AllMsgs = sort_messages(NodeDef, Store),
+    send_out_collected_messages(NodeDef, AllMsgs, AllMsgs);
+send_out_collected_messages(
+    #{<<"propertyType">> := <<"msg">>, <<"property">> := PropName} = NodeDef,
+    Store
+) ->
+    AllMsgs = sort_messages(NodeDef, Store),
+    Lst2 = [retrieve_prop_value(PropName, Msg) || Msg <- AllMsgs],
+    send_out_collected_messages(NodeDef, AllMsgs, Lst2).
+
+%%
+send_out_collected_messages(NodeDef, AllMsgs, PayloadForNodes) ->
+    %% now that we are ready to send out our message, we are completed
+    %% with the message that make up that message (!!) so those
+    %% messages should be sent to a complete node - if there is one
+    %% See this post for details:
+    %%   https://discourse.nodered.org/t/complete-node-msg-before-or-after-computation/96648/5
+    [post_completed(NodeDef, M) || M <- AllMsgs],
+
+    %% retrieve the latest message and use that as a basis for sending out
+    %% these messages - TODO perhaps this is wrong but will do for now.
+    [Msg | _] = lists:reverse(AllMsgs),
+    send_msg_to_connected_nodes(NodeDef, Msg#{?AddPayload(PayloadForNodes)}),
+
+    %% reset the store, ready to receive more messages
+    NodeDef#{'_store' => []}.
+
 %%
 %%
 convert_to_int(Val) when is_integer(Val) ->
-    {ok, Val};
+    Val;
 convert_to_int(Val) when is_float(Val) ->
-    {ok, erlang:element(1, string:to_integer(io_lib:format("~p", [Val])))};
+    erlang:element(1, string:to_integer(io_lib:format("~p", [Val])));
 convert_to_int(Val) ->
     case string:to_float(Val) of
         {error, _} ->
             case string:to_integer(Val) of
                 {error, _} ->
-                    {error, "no conversion possible"};
+                    -1;
                 {V, _} ->
-                    {ok, V}
+                    V
             end;
         {V, _} ->
             %% V is now a float and the guard 'is_float' will catch it now
-            {ok, convert_to_int(V)}
+            convert_to_int(V)
     end.
 
 %%
 %%
-use_manual(
-    <<"custom">>,
-    <<"array">>,
-    <<"msg">>,
-    Count,
-    #{<<"property">> := PropName} = NodeDef
-) ->
-    %% This means take a specific property of the msg, e.g. payload
-    %%    "mode": "custom",
-    %%    "build": "array",
-    %%    "property": "payload",
-    %%    "propertyType": "msg",
-    case convert_to_int(Count) of
-        {ok, 0} ->
-            {false, NodeDef};
-        {ok, Cnt} ->
-            {ok, NodeDef#{
-                '_is_manually_collecting' => {Cnt, {prop, PropName}, []}
-            }};
-        _ ->
-            {false, NodeDef}
-    end;
-use_manual(<<"custom">>, <<"array">>, <<"full">>, Count, NodeDef) ->
-    case convert_to_int(Count) of
-        {ok, 0} ->
-            {false, NodeDef};
-        {ok, Cnt} ->
-            {ok, NodeDef#{'_is_manually_collecting' => {Cnt, {entire_msg}, []}}};
-        _ ->
-            {false, NodeDef}
-    end;
-use_manual(_, _, _, _, NodeDef) ->
+setup_custom(
+    #{<<"propertyType">> := PropType, <<"useparts">> := UseParts} = NodeDef
+) when
+    (PropType =:= <<"full">> orelse PropType =:= <<"msg">>) andalso
+        (UseParts =:= true orelse UseParts =:= false)
+->
+    {ok, NodeDef};
+setup_custom(NodeDef) ->
     {false, NodeDef}.
+
+%%
+%%
+%% parts_sort_funct( #{ <<"parts">> := #{ <<"index">> := IndxA } },
+%%                   #{ <<"parts">> := #{ <<"index">> := IndxB } } ) ->
+%%     convert_to_int(IndxA) < convert_to_int(IndxB);
+%% parts_sort_funct(_,_) ->
+%%     true.
+
+sort_messages(#{<<"useparts">> := true}, AllMsgs) ->
+    lists:reverse(AllMsgs);
+%% see test #5cf6aec7d688fce4 but ordering is by receivership not index.
+%% lists:sort(fun parts_sort_funct/2, AllMsgs);
+sort_messages(#{<<"useparts">> := false}, AllMsgs) ->
+    lists:reverse(AllMsgs).
+
+%%
+%%
+divide_and_conquer(IdStr, Store) ->
+    divide_and_conquer(IdStr, Store, [], []).
+
+divide_and_conquer(_IdStr, [], AccWithId, AccNotId) ->
+    {AccWithId, AccNotId};
+divide_and_conquer(
+    IdStr,
+    [#{<<"parts">> := #{<<"id">> := IdStr}} = Msg | Rest],
+    AccWithId,
+    AccNotId
+) ->
+    divide_and_conquer(IdStr, Rest, [Msg | AccWithId], AccNotId);
+divide_and_conquer(IdStr, [Msg | Rest], AccWithId, AccNotId) ->
+    divide_and_conquer(IdStr, Rest, AccWithId, [Msg | AccNotId]).
+
+%%
+%%
+divide_and_conquer_no_idstr(Store) ->
+    divide_and_conquer_no_idstr(Store, [], []).
+
+divide_and_conquer_no_idstr([], AccWithId, AccWithoutId) ->
+    {AccWithId, AccWithoutId};
+divide_and_conquer_no_idstr(
+    [#{<<"parts">> := #{<<"id">> := _IdStr}} = Msg | Rest],
+    AccWithId,
+    AccWithoutId
+) ->
+    divide_and_conquer_no_idstr(Rest, [Msg | AccWithId], AccWithoutId);
+divide_and_conquer_no_idstr(
+    [Msg | Rest],
+    AccWithId,
+    AccWithoutId
+) ->
+    divide_and_conquer_no_idstr(Rest, AccWithId, [Msg | AccWithoutId]).
