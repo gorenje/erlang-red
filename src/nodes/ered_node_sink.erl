d975ad86b8b822247cd49edc7cf46fbcf32b08e0
Gerrit Riessen <gerrit@flowhub.org>
2025-08-26 15:32:36 +0200

    added sink node

---
+++ src/nodes/ered_node_sink.erl
@@ -0,0 +1,30 @@
+-module(ered_node_sink).
+
+-behaviour(ered_node).
+
+-export([start/2]).
+-export([handle_msg/2]).
+-export([handle_event/2]).
+
+%%
+%% Placeholder node where messages go to die. Sink node is a node but can
+%% be used by other nodes to ignore configurations without errors.
+%%
+
+%%
+%%
+start(NodeDef, _WsName) ->
+    ered_node:start(NodeDef, ?MODULE).
+
+%%
+%%
+handle_event(_, NodeDef) ->
+    NodeDef.
+
+%%
+%%
+handle_msg({incoming, _Msg}, NodeDef) ->
+    {handled, NodeDef, dont_send_complete_msg};
+
+handle_msg(_, NodeDef) ->
+    {unhandled, NodeDef}.
