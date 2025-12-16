a9aea7a7a241e1b07deb29bf5eb5f6c7555d31bb
João Henrique Ferreira de Freitas <joaohf@gmail.com>
2025-12-16 14:05:33 -0300

    make erlang-red cluster aware
    This is a first step for an use case where erlang-red could
    exchange messages inside a erlang-red cluster.
    
    - add rebar3 release configuration file
    - use environment variable for NODE_NAME, defaults to erlang-red
    - compute store flow path based on node name
    - add a small script to instantiate erlang-red nodes for local testing

---
+++ start-erlang-red-nodes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NODE_COUNT=${1:-1}
+
+tmux new-session -d -s erlang-red
+
+for i in `seq 0 $NODE_COUNT`; do
+  tmux split-window -t erlang-red:0 -v
+  tmux send-keys -t erlang-red:0.$i "NODE_NAME=node_$i PORT=808$i _build/default/rel/erlang_red/bin/erlang_red console" C-m
+  sleep 1
+done
+
+echo tmux attach -t erlang-red
