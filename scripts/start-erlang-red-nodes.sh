5107dfaad1b5c1d35d2dfcc122af68ab9e1255f5
Gerrit Riessen <gerrit@flowhub.org>
2025-12-16 20:27:52 +0100

    move start script to scripts

--- scripts/start-erlang-red-nodes.sh
+++ scripts/start-erlang-red-nodes.sh
@@ -6,7 +6,7 @@ tmux new-session -d -s erlang-red
 
 for i in `seq 0 $NODE_COUNT`; do
   tmux split-window -t erlang-red:0 -v
-  tmux send-keys -t erlang-red:0.$i "NODE_NAME=node_$i PORT=808$i _build/default/rel/erlang_red/bin/erlang_red console" C-m
+  tmux send-keys -t erlang-red:0.$i "NODE_NAME=node_$i PORT=808$i ../_build/default/rel/erlang_red/bin/erlang_red console" C-m
   sleep 1
 done
 
