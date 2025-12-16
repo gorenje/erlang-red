cf5554d031ad56190309677da39371071e7003a7
Gerrit Riessen <gerrit@flowhub.org>
2025-12-16 21:26:06 +0100

    flow store: remove the node from the path and added a flow search for existing flows in the testflows directory

--- src/ered_flow_store.erl
+++ src/ered_flow_store.erl
@@ -1,20 +1,41 @@
 -module(ered_flow_store).
 
--export([store_flow/0, store_flow_id/1, store_main_flow/0]).
+-export([
+    store_flow/0,
+    store_flow_id/1,
+    store_main_flow/0,
+    find_flow_id/1
+]).
 
 store_flow_id(FlowId) when is_binary(FlowId) ->
     store_flow_id(binary_to_list(FlowId));
 store_flow_id(FlowId) when is_list(FlowId) ->
-    Node = erlang:node(),
-    filename:join([get_store_flow(), Node, FlowId, "flows.json"]).
+    filename:join([get_store_flow(), FlowId, "flows.json"]).
+
+%%
+%% Allow flowhub pull to use the existing testflows. But if the flow
+%% has been stored, then use that version.
+find_flow_id(FlowId) when is_binary(FlowId) ->
+    find_flow_id(binary_to_list(FlowId));
+find_flow_id(FlowId) when is_list(FlowId) ->
+    FileName = store_flow_id(FlowId),
+    case filelib:is_file(FileName) of
+        true ->
+            FileName;
+        _ ->
+            filename:join([
+                code:priv_dir(erlang_red),
+                "testflows",
+                FlowId,
+                "flows.json"
+            ])
+    end.
 
 store_flow() ->
-    Node = erlang:node(),
-    filename:join(get_store_flow(), Node).
+    get_store_flow().
 
 store_main_flow() ->
-    Node = erlang:node(),
-    filename:join([get_store_flow(), Node, "flows.json"]).
+    filename:join([get_store_flow(), "flows.json"]).
 
 get_store_flow() ->
     application:get_env(
